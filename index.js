93780b683bc3e10c548a4ff3b791064e7157408c
Anson Chan <anson0370@gmail.com>
2021-03-29 10:24:42 +0800

    fix: fastjson bug

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function parse(jsonString) {
 function prepareJsonString(jsonString) {
   // 这里应该是 fastjson 的一个 bug ，对 __xxx 的字段在生成 $ref 的时候变成了 \\_\\_ ，普通的 value 不会
   // 所以这里替换一下
-  return jsonString.replace(/\\_/g, '_');
+  return jsonString.replace(/\\\\_/g, '_');
 }
 
 function visitJsonObj(jsonObj, parentObj, selfPath, refTargets, currentPath) {
